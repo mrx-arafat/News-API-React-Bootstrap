4e49301255f62d8e138c8c37310256ee3b076c95
Easin Arafat <arafatmrx@gmail.com>
2021-10-03 10:10:13 +0600

    data show

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
+import News from "./components/News/News";
 
 function App() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     fetch(
-      "https://newsapi.org/v2/everything?q=tesla&from=2021-09-03&sortBy=publishedAt&apiKey=e976f01f0de042259db708a7e4b427f0"
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e976f01f0de042259db708a7e4b427f0"
     )
       .then((res) => res.json())
       .then((data) => setNews(data.articles));
   }, []);
   return (
     <Row xs={1} md={2} className="g-4">
-      {news.map()}
+      {news.map((nw) => (
+        <News news={nw}></News>
+      ))}
     </Row>
   );
 }
