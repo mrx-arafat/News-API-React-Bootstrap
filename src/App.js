f42776923cdb87eed9f7909a339c6939d533d3c1
Easin Arafat <arafatmrx@gmail.com>
2021-10-03 09:45:08 +0600

    news fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Card, Col, Row } from "react-bootstrap";
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=tesla&from=2021-09-03&sortBy=publishedAt&apiKey=e976f01f0de042259db708a7e4b427f0"
+    )
+      .then((res) => res.json())
+      .then((data) => setNews(data.articles));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row xs={1} md={2} className="g-4">
+      {news.map()}
+    </Row>
   );
 }
 
